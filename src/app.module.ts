c3142ea0ba53101cca8dda219b6ff6113b3b3e1d
neil-toscano <neil.toscano.f@uni.pe>
2024-11-06 08:47:01 -0500

    hotfix-ssl

--- src/app.module.ts
+++ src/app.module.ts
@@ -30,10 +30,10 @@ import { UserPermissionsModule } from './user-permissions/user-permissions.modul
     ConfigModule.forRoot(),
 
     TypeOrmModule.forRoot({
-      ssl: true,
-      extra: {
-        ssl: true ? { rejectUnauthorized: false } : null,
-      },
+      // ssl: true,
+      // extra: {
+      //   ssl: true ? { rejectUnauthorized: false } : null,
+      // },
       type: 'postgres',
       host: process.env.DB_HOST,
       port: +process.env.DB_PORT,
