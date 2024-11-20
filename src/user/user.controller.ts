b4afa3f31f544b43166a89cc50040975a4dbe065
neil-toscano <neil.toscano.f@uni.pe>
2024-11-20 16:53:14 -0500

    cambios-permisos-user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -35,7 +35,7 @@ export class UserController {
   }
 
   @Patch(':id')
-  @Auth(ValidRoles.admin)
+  @Auth()
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.userService.update(id, updateUserDto);
   }
