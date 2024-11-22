b2eef8b7ff8a5db825a0e87700e8288a373e31a8
neil-toscano <neil.toscano.f@uni.pe>
2024-11-22 16:05:38 -0500

    cambios-upload-pdf

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -8,145 +8,53 @@ import {
   BadRequestException,
   Res,
 } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { Response } from 'express';
 import { diskStorage } from 'multer';
 import { FilesService } from './files.service';
 
-import { fileFilter, fileNamer } from './helpers';
+import { fileNamer } from './helpers';
 import { Auth } from 'src/auth/decorators';
-import { existsSync, readdirSync } from 'fs';
-import { join } from 'path';
 
 @Controller('files')
 export class FilesController {
   constructor(private readonly filesService: FilesService) {}
 
-  // @Post('pdf')
-  // @Auth()
-  // @UseInterceptors(
-  //   FileInterceptor('file', {
-  //     fileFilter: (req, file, callback) => {
-  //       if (!file.originalname.match(/\.(pdf)$/)) {
-  //         return callback(
-  //           new BadRequestException('Only PDF files are allowed!'),
-  //           false,
-  //         );
-  //       }
-  //       callback(null, true);
-  //     },
-  //     storage: diskStorage({
-  //       // destination: './static/pdf', // Asegúrate de tener este directorio para almacenar PDFs
-  //       destination: 'http://172.16.23.11/DATOS/BACKUP TAREAJE/sistema_fisca', // Asegúrate de tener este directorio para almacenar PDFs
-  //       filename: fileNamer,
-  //     }),
-  //   }),
-  // )
-  // uploadDocumentPDF(@UploadedFile() file: Express.Multer.File) {
-  //   if (!file) {
-  //     throw new BadRequestException('Make sure that the file is a PDF');
-  //   }
-
-  //   const fileUrl = `${file.filename}`;
-
-  //   return { fileUrl };
-  // }
-
-@Post('pdf')
-@Auth()
-@UseInterceptors(
-  FileInterceptor('file', {
-    fileFilter: (req, file, callback) => {
-      if (!file.originalname.match(/\.(pdf)$/)) {
-        return callback(
-          new BadRequestException('Only PDF files are allowed!'),
-          false,
-        );
-      }
-      callback(null, true);
-    },
-    storage: diskStorage({
-      // Ruta de red válida para guardar el archivo
-      destination: '\\172.16.23.11\\DATOS\\BACKUP TAREAJE\\gestion_citas',
-      filename: (req, file, callback) => {
-        const timestamp = Date.now(); // Evitar colisiones usando un timestamp
-        const uniqueName = `${timestamp}-${file.originalname}`;
-        callback(null, uniqueName);
+  @Post('pdf')
+  @Auth()
+  @UseInterceptors(
+    FileInterceptor('file', {
+      fileFilter: (req, file, callback) => {
+        if (!file.originalname.match(/\.(pdf)$/)) {
+          return callback(
+            new BadRequestException('Only PDF files are allowed!'),
+            false,
+          );
+        }
+        callback(null, true);
       },
+      storage: diskStorage({
+        destination: './static/pdf', // Asegúrate de tener este directorio para almacenar PDFs
+        filename: fileNamer,
+      }),
     }),
-  }),
-)
-uploadDocumentPDF(@UploadedFile() file: Express.Multer.File) {
-  if (!file) {
-    throw new BadRequestException('Make sure that the file is a PDF');
-  }
-
-  // Devolver la ruta completa donde se guardó el archivo
-  const filePath = `\\\\172.16.23.11\\DATOS\\BACKUP TAREAJE\\gestion_citas\\${file.filename}`;
-
-  return { message: 'Archivo subido exitosamente', filePath };
-}
-
-
-
-  // @Get('pdf/:pdfName')
-  // getFile(@Res() res: Response, @Param('pdfName') pdfName: string) {
-  //   const path = this.filesService.getFile(pdfName);
-
-  //   res.sendFile(path);
-  // }
-  @Get('pdf')
-listPdfs() {
-  // Ruta de la carpeta compartida
-  const directoryPath = '\\172.16.23.11\\DATOS\\BACKUP TAREAJE\\gestion_citas';
-
-  try {
-    // Leer el contenido del directorio
-    const files = readdirSync(directoryPath);
-
-    // Filtrar para asegurarnos de que sean solo archivos PDF
-    const pdfFiles = files.filter(file => file.endsWith('.pdf'));
-
-    return { message: 'Archivos encontrados', files: pdfFiles };
-  } catch (error) {
-    throw new BadRequestException('Error al leer la carpeta o no se encontró');
-  }
-}
-
-@Get('pdf/:pdfName')
-getPdfByName(@Param('pdfName') pdfName: string, @Res() res: Response) {
-  // Ruta de la carpeta compartida
-  const directoryPath = '\\\\172.16.23.11\\DATOS\\BACKUP TAREAJE\\gestion_citas';
-
-  try {
-    // Leer el contenido del directorio
-    const files = readdirSync(directoryPath);
-
-    // Buscar el archivo específico que coincida con el nombre
-    const pdfFile = files.find(file => file === pdfName);
-
-    if (!pdfFile) {
-      throw new BadRequestException(`No se encontró el archivo: ${pdfName}`);
+  )
+  uploadDocumentPDF(@UploadedFile() file: Express.Multer.File) {
+    if (!file) {
+      throw new BadRequestException('Make sure that the file is a PDF');
     }
 
-    // Construir la ruta completa del archivo
-    const filePath = join(directoryPath, pdfFile);
+    const fileUrl = `${file.filename}`;
 
-    // Enviar el archivo PDF al cliente
-    res.sendFile(filePath, (err) => {
-      if (err) {
-        throw new BadRequestException('Error al enviar el archivo');
-      }
-    });
-
-  } catch (error) {
-    console.log(error);
-    throw new BadRequestException('Error al leer la carpeta o no se encontró el archivo');
+    return { fileUrl };
   }
-}
 
+  @Get('pdf/:pdfName')
+  getFile(@Res() res: Response, @Param('pdfName') pdfName: string) {
+    const path = this.filesService.getFile(pdfName);
 
+    res.sendFile(path);
+  }
 }
 
 
--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -1,4 +1,4 @@
-import { existsSync, readdirSync, unlinkSync } from 'fs';
+import { existsSync, unlinkSync } from 'fs';
 import { join } from 'path';
 
 import { Injectable, BadRequestException } from '@nestjs/common';
@@ -7,34 +7,11 @@ import { Injectable, BadRequestException } from '@nestjs/common';
 export class FilesService {
   constructor() {}
 
-  // getFile(filename: string) {
-  //   // const path = join(__dirname, '../../static/pdf', filename);
-  //   const path = join('http://172.16.23.11/DATOS/BACKUP TAREAJE', 'sistema_fisca', filename);
-
-  //   if (!existsSync(path))
-  //     throw new BadRequestException(`No file(pdf) found with name ${filename}`);
-  //   return path;
-  // }
-
-  getFile(filename: string): string {
-    // Validar que el nombre sea seguro (opcional)
-    if (!/^[a-zA-Z0-9._-]+\.pdf$/.test(filename)) {
-      throw new BadRequestException('Invalid file name');
-    }
-
-    // Construir la ruta completa
-    const path = join(
-      '\\\\172.16.23.11\\DATOS\\BACKUP TAREAJE',
-      'sistema_fisca',
-      filename
-    );
-
-    // Verificar si el archivo existe
-    if (!existsSync(path)) {
-      throw new BadRequestException(`No file (PDF) found with name ${filename}`);
-    }
-
-    return path; // Devolver la ruta completa
+  getFile(filename: string) {
+    const path = join(__dirname, '../../static/pdf', filename);
+    if (!existsSync(path))
+      throw new BadRequestException(`No file(pdf) found with name ${filename}`);
+    return path;
   }
 
   deleteFile(filename: string) {
@@ -51,19 +28,5 @@ export class FilesService {
       console.log(error.message, filename);
       throw new BadRequestException(`Error al encontrar el archivo`);
     }
-  }
-
-  async getAllPdfFiles() {
-    const pdfDirectory = join(__dirname, '../../static/pdf');
-
-    if (!existsSync(pdfDirectory)) {
-      throw new BadRequestException(`Directory not found: ${pdfDirectory}`);
-    }
-
-    const files = readdirSync(pdfDirectory);
-
-    const pdfFiles = files.filter((file) => file.endsWith('.pdf'));
-
-    return pdfFiles;
   }
 }
